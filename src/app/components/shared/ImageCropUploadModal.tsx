5bde046963cdcd3cb1c48e4e3f5c488992bf6195
Alfonso Germano <alfonso.germano@gmail.com>
2026-06-30 19:38:40 +0200

    chore: rimuove log di debug CROP-DEBUG dal modal di ritaglio immagine

--- src/app/components/shared/ImageCropUploadModal.tsx
+++ src/app/components/shared/ImageCropUploadModal.tsx
@@ -20,10 +20,9 @@ const MAX_IMAGE_MB = 5;
 async function getCroppedBlob(imageSrc: string, area: Area): Promise<Blob> {
   const image = await new Promise<HTMLImageElement>((resolve, reject) => {
     const img = new Image();
-    console.log('[CROP-DEBUG] inizio caricamento immagine, src=', imageSrc.slice(0, 50));
     img.crossOrigin = 'anonymous';
-    img.onload = () => { console.log('[CROP-DEBUG] onload scattato'); resolve(img); };
-    img.onerror = (e) => { console.log('[CROP-DEBUG] onerror scattato', e); reject(e); };
+    img.onload = () => resolve(img);
+    img.onerror = reject;
     img.src = imageSrc;
   });
   const canvas = document.createElement('canvas');
