b2077a9c366c63f48d10e43a662814ef0edc3cd5
alfonsogermano-glitch <alfonso.germano@gmail.com>
2026-09-06 17:54:38 +0200

    fix: embed exact Stone texture shards

--- src/app/components/session/dice/stoneTextureData.ts
+++ src/app/components/session/dice/stoneTextureData.ts
@@ -2,8 +2,17 @@ import { STONE_TEXTURE_CHUNK_0 } from './stoneTextureChunk0.ts';
 import { STONE_TEXTURE_CHUNK_1 } from './stoneTextureChunk1.ts';
 import { STONE_TEXTURE_CHUNK_2 } from './stoneTextureChunk2.ts';
 import { STONE_TEXTURE_CHUNK_3 } from './stoneTextureChunk3.ts';
+import { STONE_TEXTURE_CHUNK_4 } from './stoneTextureChunk4.ts';
+import { STONE_TEXTURE_CHUNK_5 } from './stoneTextureChunk5.ts';
+import { STONE_TEXTURE_CHUNK_6 } from './stoneTextureChunk6.ts';
+import { STONE_TEXTURE_CHUNK_7 } from './stoneTextureChunk7.ts';
+import { STONE_TEXTURE_CHUNK_8 } from './stoneTextureChunk8.ts';
+import { STONE_TEXTURE_CHUNK_9 } from './stoneTextureChunk9.ts';
+import { STONE_TEXTURE_CHUNK_10 } from './stoneTextureChunk10.ts';
+import { STONE_TEXTURE_CHUNK_11 } from './stoneTextureChunk11.ts';
+import { STONE_TEXTURE_CHUNK_12 } from './stoneTextureChunk12.ts';
 
-const STONE_TEXTURE_BASE64 = `${STONE_TEXTURE_CHUNK_0}${STONE_TEXTURE_CHUNK_1}${STONE_TEXTURE_CHUNK_2}${STONE_TEXTURE_CHUNK_3}`;
+const STONE_TEXTURE_BASE64 = `${STONE_TEXTURE_CHUNK_0}${STONE_TEXTURE_CHUNK_1}${STONE_TEXTURE_CHUNK_2}${STONE_TEXTURE_CHUNK_3}${STONE_TEXTURE_CHUNK_4}${STONE_TEXTURE_CHUNK_5}${STONE_TEXTURE_CHUNK_6}${STONE_TEXTURE_CHUNK_7}${STONE_TEXTURE_CHUNK_8}${STONE_TEXTURE_CHUNK_9}${STONE_TEXTURE_CHUNK_10}${STONE_TEXTURE_CHUNK_11}${STONE_TEXTURE_CHUNK_12}`;
 
 export const STONE_TEXTURE_SOURCE_DATA_URL = `data:image/webp;base64,${STONE_TEXTURE_BASE64}`;
 export const STONE_TEXTURE_DATA_URL = STONE_TEXTURE_SOURCE_DATA_URL;
