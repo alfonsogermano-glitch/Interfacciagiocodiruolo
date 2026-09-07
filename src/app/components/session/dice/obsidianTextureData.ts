304560564b6a29c87f914ec9973f9f645164e212
alfonsogermano-glitch <alfonso.germano@gmail.com>
2026-09-07 12:51:03 +0200

    feat: embed final Obsidian photographic texture

--- src/app/components/session/dice/obsidianTextureData.ts
+++ src/app/components/session/dice/obsidianTextureData.ts
@@ -1,7 +1,15 @@
 import { OBSIDIAN_TEXTURE_CHUNK_0 } from './obsidianTextureChunk0.ts';
 import { OBSIDIAN_TEXTURE_CHUNK_1 } from './obsidianTextureChunk1.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_2 } from './obsidianTextureChunk2.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_3 } from './obsidianTextureChunk3.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_4 } from './obsidianTextureChunk4.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_5 } from './obsidianTextureChunk5.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_6 } from './obsidianTextureChunk6.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_7 } from './obsidianTextureChunk7.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_8 } from './obsidianTextureChunk8.ts';
+import { OBSIDIAN_TEXTURE_CHUNK_9 } from './obsidianTextureChunk9.ts';
 
-const OBSIDIAN_TEXTURE_BASE64 = `${OBSIDIAN_TEXTURE_CHUNK_0}${OBSIDIAN_TEXTURE_CHUNK_1}`;
+const OBSIDIAN_TEXTURE_BASE64 = `${OBSIDIAN_TEXTURE_CHUNK_0}${OBSIDIAN_TEXTURE_CHUNK_1}${OBSIDIAN_TEXTURE_CHUNK_2}${OBSIDIAN_TEXTURE_CHUNK_3}${OBSIDIAN_TEXTURE_CHUNK_4}${OBSIDIAN_TEXTURE_CHUNK_5}${OBSIDIAN_TEXTURE_CHUNK_6}${OBSIDIAN_TEXTURE_CHUNK_7}${OBSIDIAN_TEXTURE_CHUNK_8}${OBSIDIAN_TEXTURE_CHUNK_9}`;
 
 export const OBSIDIAN_TEXTURE_SOURCE_DATA_URL = `data:image/webp;base64,${OBSIDIAN_TEXTURE_BASE64}`;
 export const OBSIDIAN_TEXTURE_DATA_URL = OBSIDIAN_TEXTURE_SOURCE_DATA_URL;
